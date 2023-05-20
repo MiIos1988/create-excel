04806f088c712e71ceff513f6aa4a21a34780422
Milos1988 <vojvoda1988@gmail.com>
2023-05-20 02:09:29 +0200

    Create exel table for all name

--- exel.js
+++ exel.js
@@ -7,60 +7,60 @@ const filePaths = ['/home/milos/Downloads/output.xlsx', '/home/milos/Downloads/o
 
 // Funkcija za konverziju broja kolone u ime kolone
 function columnNumberToName(columnNumber) {
-    let columnName = '';
-    while (columnNumber > 0) {
-      const remainder = (columnNumber - 1) % 26;
-      columnName = String.fromCharCode(65 + remainder) + columnName;
-      columnNumber = Math.floor((columnNumber - 1) / 26);
-    }
-    return columnName;
+  let columnName = '';
+  while (columnNumber > 0) {
+    const remainder = (columnNumber - 1) % 26;
+    columnName = String.fromCharCode(65 + remainder) + columnName;
+    columnNumber = Math.floor((columnNumber - 1) / 26);
   }
-  
-  // Funkcija za izvlačenje imena iz fajlova i generisanje novih fajlova
-  async function extractNamesAndCreateFiles(filePaths) {
-    const firstFilePath = filePaths[0];
-    const folderPath = path.dirname(firstFilePath);
-    const newFolderPath = path.join(folderPath, 'Refundacije');
-  
-    // Kreiranje novog foldera
-    fs.mkdirSync(newFolderPath, { recursive: true });
-  
-    // Kopiranje rasporeda kolona iz prvog fajla
-    const firstWorkbook = await XlsxPopulate.fromFileAsync(firstFilePath);
-    const firstSheet = firstWorkbook.sheet(0);
-    const columnCount = firstSheet.usedRange().endCell().columnNumber();
-  
-    for (let i = 0; i < filePaths.length; i++) {
-      const filePath = filePaths[i];
-      const workbook = await XlsxPopulate.fromFileAsync(filePath);
-      const sheet = workbook.sheet(0);
-  
-      const rowCount = sheet.usedRange().endCell().rowNumber();
-      for (let j = 2; j <= rowCount; j++) {
-        const cell = sheet.cell(`A${j}`);
-        const name = cell.value();
-  
-        const newFilePath = path.join(newFolderPath, `${name}.xlsx`);
-  
-        const newWorkbook = await XlsxPopulate.fromBlankAsync();
-        const newSheet = newWorkbook.sheet(0);
-  
-        // Kopiranje rasporeda kolona iz prvog fajla u novi fajl
-        for (let k = 1; k <= columnCount; k++) {
-          const columnLetter = columnNumberToName(k);
-          const firstCell = firstSheet.cell(`${columnLetter}1`);
-          const value = firstCell.value();
-          newSheet.cell(`${columnLetter}1`).value(value);
-        }
-        await newWorkbook.toFileAsync(newFilePath);
+  return columnName;
+}
+
+// Funkcija za izvlačenje imena iz fajlova i generisanje novih fajlova
+async function extractNamesAndCreateFiles(filePaths) {
+  const firstFilePath = filePaths[0];
+  const folderPath = path.dirname(firstFilePath);
+  const newFolderPath = path.join(folderPath, 'Refundacije');
+
+  // Kreiranje novog foldera
+  fs.mkdirSync(newFolderPath, { recursive: true });
+
+  // Kopiranje rasporeda kolona iz prvog fajla
+  const firstWorkbook = await XlsxPopulate.fromFileAsync(firstFilePath);
+  const firstSheet = firstWorkbook.sheet(0);
+  const columnCount = firstSheet.usedRange().endCell().columnNumber();
+
+  for (let i = 0; i < filePaths.length; i++) {
+    const filePath = filePaths[i];
+    const workbook = await XlsxPopulate.fromFileAsync(filePath);
+    const sheet = workbook.sheet(0);
+
+    const rowCount = sheet.usedRange().endCell().rowNumber();
+    for (let j = 2; j <= rowCount; j++) {
+      const cell = sheet.cell(`A${j}`);
+      const name = cell.value();
+
+      const newFilePath = path.join(newFolderPath, `${name}.xlsx`);
+
+      const newWorkbook = await XlsxPopulate.fromBlankAsync();
+      const newSheet = newWorkbook.sheet(0);
+
+      // Kopiranje rasporeda kolona iz prvog fajla u novi fajl
+      for (let k = 1; k <= columnCount; k++) {
+        const columnLetter = columnNumberToName(k);
+        const firstCell = firstSheet.cell(`${columnLetter}1`);
+        const value = firstCell.value();
+        newSheet.cell(`${columnLetter}1`).value(value);
       }
+      await newWorkbook.toFileAsync(newFilePath);
     }
   }
-  // Izvlačenje imena iz fajlova i generisanje novih fajlova
+}
+// Izvlačenje imena iz fajlova i generisanje novih fajlova
 extractNamesAndCreateFiles(filePaths)
-.then(() => {
-  console.log('Generisanje fajlova je završeno.');
-})
-.catch((error) => {
-  console.error('Došlo je do greške prilikom generisanja fajlova:', error);
-});
+  .then(() => {
+    console.log('Generisanje fajlova je završeno.');
+  })
+  .catch((error) => {
+    console.error('Došlo je do greške prilikom generisanja fajlova:', error);
+  });
